afe5e5bda49b5bab7c2f32c44b9be30e92003af1
giuoaejgiusejnb <252497528+giuoaejgiusejnb@users.noreply.github.com>
2026-05-05 09:03:36 +0900

    ログインと新規登録時，ログイン処理中，新規登録中と表示するように変更

--- src/views/login_view.py
+++ src/views/login_view.py
@@ -4,7 +4,8 @@ import uuid
 import flet as ft
 from components import (
     create_password_field,
-    PW_FIELD_WIDTH
+    PW_FIELD_WIDTH,
+    use_status_text
 )
 from constants import (
     GUEST_USER_NAME,
@@ -32,22 +33,20 @@ def LoginView(page: TypedPage) -> ft.Control:
         []
     )
     # ログインや新規登録失敗時に表示するメッセージ
-    # TODO エラーではなく，ステータスメッセージ（ログイン中など）を追加
-    error_str, set_error_str = ft.use_state("")
-
-    error_text = ft.Text(error_str, color="red")
+    login_status_control, set_login_status = use_status_text()
 
     async def login(name: str, pwd: str)-> None:
+        set_login_status("ログイン処理中...", ft.Colors.GREEN)
         email = await user_repo.get_email_by_name(name)
         if email is None:
-            set_error_str("ユーザー名が間違えています")
+            set_login_status("ユーザー名が間違えています", ft.Colors.RED)
             return
 
         try:
             user = await page.app_state.auth.login(email=email, password=pwd)
             await page.app_state.login(uid=user.uid, name=name)
         except Exception as e:
-            set_error_str(FIREBASE_ERR_MSGS.get(str(e), f"エラー：{e}"))
+            set_login_status(FIREBASE_ERR_MSGS.get(str(e), f"エラー：{e}"), ft.Colors.RED)
 
     async def login_click(e: ft.ControlEvent) -> None:
         await login(user_name_field.value, password_field.value)
@@ -56,21 +55,22 @@ def LoginView(page: TypedPage) -> ft.Control:
         await login(GUEST_USER_NAME, GUEST_PASSWORD)
 
     async def register_click(e: ft.ControlEvent)-> None:
+        set_login_status("新規登録中...", ft.Colors.GREEN)
         user_name = user_name_field.value
         if user_name == "":
-            set_error_str("ユーザー名を入力してください")
+            set_login_status("ユーザー名を入力してください", ft.Colors.RED)
             return
         password = password_field.value
         res = PasswordValidator().check(password)
         if not res.is_valid:
-            set_error_str(res.join_errors())
+            set_login_status(res.join_errors(), ft.Colors.RED)
             return
 
         fixed_id = uuid.uuid4().hex
         dummy_email = f"{fixed_id}@mhrsb-app.internal"
         is_username_taken = await user_repo.is_username_taken(user_name)
         if is_username_taken:
-            set_error_str("そのユーザー名は既に存在しています")
+            set_login_status("そのユーザー名は既に存在しています", ft.Colors.RED)
             return
 
         try:
@@ -79,7 +79,7 @@ def LoginView(page: TypedPage) -> ft.Control:
             await AuthService(page.app_state.repos).register_new_user(uid, user_name, dummy_email)
         except Exception as ex:
         # すでに登録されている場合やパスワードが短い場合などにエラー
-            set_error_str(FIREBASE_ERR_MSGS.get(str(ex), f"エラー：{ex}"))
+            set_login_status(FIREBASE_ERR_MSGS.get(str(ex), f"エラー：{ex}"), ft.Colors.RED)
         else:
             # 登録成功後、そのままログイン状態にしてダッシュボードへ
             await page.app_state.login(uid, user_name)
@@ -90,5 +90,5 @@ def LoginView(page: TypedPage) -> ft.Control:
         ft.Button("ログイン", on_click=login_click),
         ft.Button("ゲストでログイン", on_click=guest_login_click ),
         ft.Button("新規登録", on_click=register_click, bgcolor="green", color="white"),
-        error_text
+        login_status_control
     ],)
