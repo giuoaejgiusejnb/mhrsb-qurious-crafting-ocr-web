fe06f32bda661f650accd9e396dec325a963d79e
giuoaejgiusejnb <252497528+giuoaejgiusejnb@users.noreply.github.com>
2026-05-06 23:33:59 +0900

    Ruffを適用

--- src/repositories/qc_settings_repository.py
+++ src/repositories/qc_settings_repository.py
@@ -1,16 +1,16 @@
 from google.cloud.firestore import AsyncClient, AsyncWriteBatch
-from pydantic import BaseModel, Field, ConfigDict
-from constants import (
-    COL_USERS,
-    COL_DESIRED_SKILLS_SETTINGS,
-    FIELD_SKILLS
-)
+from pydantic import BaseModel, ConfigDict, Field
+
+from constants import COL_DESIRED_SKILLS_SETTINGS, COL_USERS, FIELD_SKILLS
+
 from .base_repository import BaseRepository
+
 
 # --- QC設定を格納するクラス ---
 class QCSettings(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
     skills: set[str] | None = Field(None, alias=FIELD_SKILLS)
+
 
 # --- QC設定を扱うリポジトリ ---
 class QCSettingsRepository(BaseRepository[QCSettings]):
@@ -24,8 +24,7 @@ class QCSettingsRepository(BaseRepository[QCSettings]):
     # QC設定を取得
     async def fetch(self, user_id: str, settings_name: str) -> QCSettings:
         return await self.get(
-            collection_path=self.get_collection_path(user_id),
-            document_id=settings_name
+            collection_path=self.get_collection_path(user_id), document_id=settings_name
         )
 
     # ユーザーが保存しているQC設定の名前をすべて取得
@@ -34,7 +33,13 @@ class QCSettingsRepository(BaseRepository[QCSettings]):
         return [doc_id for doc_id, _ in docs]
 
     # QC設定を保存・更新
-    async def update(self, user_id: str, settings_name:str, settings: QCSettings, batch: AsyncWriteBatch | None = None) -> None:
+    async def update(
+        self,
+        user_id: str,
+        settings_name: str,
+        settings: QCSettings,
+        batch: AsyncWriteBatch | None = None,
+    ) -> None:
         # 更新時は未設定(unset)のものを除外し、設定したい差分だけをマージする
         data = settings.model_dump(by_alias=True, exclude_unset=True)
         await self.save(
@@ -42,13 +47,15 @@ class QCSettingsRepository(BaseRepository[QCSettings]):
             document_id=settings_name,
             data=data,
             merge=True,
-            batch=batch
+            batch=batch,
         )
-        
+
     # QC設定を削除
-    async def delete_settings(self, user_id: str, settings_name: str, batch: AsyncWriteBatch | None = None) -> None:
+    async def delete_settings(
+        self, user_id: str, settings_name: str, batch: AsyncWriteBatch | None = None
+    ) -> None:
         await self.delete_doc(
             collection_path=self.get_collection_path(user_id),
             document_id=settings_name,
-            batch=batch
+            batch=batch,
         )
