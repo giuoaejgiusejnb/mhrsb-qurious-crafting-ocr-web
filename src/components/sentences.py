8d4eaf1c27fcf1ee0c535f4734f25dd37abc0d2e
giuoaejgiusejnb <252497528+giuoaejgiusejnb@users.noreply.github.com>
2026-06-23 14:59:56 +0900

    右端に行ったときに改行できるよう，自作クラスではなく，ft.Rowを使うように変更

--- src/components/sentences.py
+++ src/components/sentences.py
@@ -1,21 +1,23 @@
-import flet as ft
-import nbformat
-import httpx
 import re
+
+import flet as ft
+import httpx
+import nbformat
+
+from components.status_text import use_status_text
 from constants import (
-    OCR_NOTEBOOK_PATH,
     APP_NAME,
     ENDPOINT_QC_LOG,
+    OCR_NOTEBOOK_PATH,
     SKILL_MASTER_LIST,
 )
-from .linked_sentence import LinkedSentence
 from models.app_state import TypedPage
-from components.status_text import use_status_text
 
-def get_open_colab_sentence(page:TypedPage, settings_name: str, input_zip_file: str):
+
+def get_open_colab_sentence(page: TypedPage, settings_name: str, input_zip_file: str):
     user_id = page.app_state.user_id
     qc_settings_repo = page.app_state.repos.qc_settings_repo
-    
+
     colab_url, set_colab_url = ft.use_state("")
     url_status_control, set_url_status = use_status_text()
 
@@ -23,10 +25,14 @@ def get_open_colab_sentence(page:TypedPage, settings_name: str, input_zip_file: 
         set_url_status("Colab用URLを作成中...", "blue")
         set_colab_url("")
         if settings_name == "" or input_zip_file == "":
-            set_url_status("欲しいスキル設定か画像zipファイルの名前が設定されていません", "red")
+            set_url_status(
+                "欲しいスキル設定か画像zipファイルの名前が設定されていません", "red"
+            )
             return
 
-        qc_settings = await qc_settings_repo.fetch(user_id=user_id, settings_name=settings_name)
+        qc_settings = await qc_settings_repo.fetch(
+            user_id=user_id, settings_name=settings_name
+        )
         desired_skills = qc_settings.skills
 
         # colabの変数の値を書き換える
@@ -35,7 +41,7 @@ def get_open_colab_sentence(page:TypedPage, settings_name: str, input_zip_file: 
             "fly_url": f"https://{APP_NAME}.fly.dev{ENDPOINT_QC_LOG}",
             "skill_master_list": SKILL_MASTER_LIST,
             "desired_skills": desired_skills,
-            "input_zip_file": input_zip_file
+            "input_zip_file": input_zip_file,
         }
         with open(OCR_NOTEBOOK_PATH, "r", encoding="utf-8") as f:
             nb = nbformat.read(f, as_version=4)
@@ -56,15 +62,22 @@ def get_open_colab_sentence(page:TypedPage, settings_name: str, input_zip_file: 
                 pattern = rf"^{var_name}\s*=.*"
                 replacement = f"{var_name} = {repr(new_value)}"
 
-                target_cell.source = re.sub(pattern, replacement, target_cell.source, flags=re.MULTILINE)
+                target_cell.source = re.sub(
+                    pattern, replacement, target_cell.source, flags=re.MULTILINE
+                )
 
         gist_filename = "temp_modified"
         try:
-            res_data = await page.app_state.shared_gist.upload_ipynb(content=nbformat.writes(nb), file_name=gist_filename)
+            res_data = await page.app_state.shared_gist.upload_ipynb(
+                content=nbformat.writes(nb), file_name=gist_filename
+            )
             gist_id = res_data["id"]
             gist_username = res_data["owner"]["login"]
         except httpx.HTTPStatusError as ex:
-            set_url_status(f"Gistへのアップロードに失敗しました: {ex.response.status_code} {ex.response.text}", "red")
+            set_url_status(
+                f"Gistへのアップロードに失敗しました: {ex.response.status_code} {ex.response.text}",
+                "red",
+            )
             return
         else:
             # TODO ユーザーごとにgistを一個作って，消さないようにする？
@@ -74,48 +87,74 @@ def get_open_colab_sentence(page:TypedPage, settings_name: str, input_zip_file: 
             set_url_status("Colab用URLを作成しました", "green")
 
     def open_how_to_enable_gpu_dialog(e):
-        e.page.show_dialog(ft.AlertDialog(
-            title=ft.Text("GPUを有効にする方法"),
-            content=ft.Column(
-                controls=[
-                    ft.Text("（（スマホの場合は≡を押して，）Google Colabのメニューから，ランタイム > ランタイムのタイプを変更 を選択してください"),
-                    ft.Text("ハードウェアアクセラレータのところでGPUを選択して保存をクリックしてください"),
+        e.page.show_dialog(
+            ft.AlertDialog(
+                title=ft.Text("GPUを有効にする方法"),
+                content=ft.Column(
+                    controls=[
+                        ft.Text(
+                            "（（スマホの場合は≡を押して，）Google Colabのメニューから，ランタイム > ランタイムのタイプを変更 を選択してください"
+                        ),
+                        ft.Text(
+                            "ハードウェアアクセラレータのところでGPUを選択して保存をクリックしてください"
+                        ),
+                    ],
+                    tight=True,
+                ),
+                actions=[
+                    ft.TextButton("閉じる", on_click=lambda e: e.page.pop_dialog())
                 ],
-                tight=True,
-            ),
-            actions=[ft.TextButton("閉じる", on_click=lambda e: e.page.pop_dialog())]
-        ))
+            )
+        )
 
     def open_how_to_run_cells_dialog(e):
-        e.page.show_dialog(ft.AlertDialog(
-            title=ft.Text("すべてのセルを実行する方法"),
-            content=ft.Column(
-                controls=[
-                    ft.Text("（（スマホの場合は≡を押して，）Google Colabのメニューから，ランタイム > すべてのセルを実行 を選択してください"),
-                    ft.Text("あとはしばらく待つだけです。"),
+        e.page.show_dialog(
+            ft.AlertDialog(
+                title=ft.Text("すべてのセルを実行する方法"),
+                content=ft.Column(
+                    controls=[
+                        ft.Text(
+                            "（（スマホの場合は≡を押して，）Google Colabのメニューから，ランタイム > すべてのセルを実行 を選択してください"
+                        ),
+                        ft.Text("あとはしばらく待つだけです。"),
+                    ],
+                    tight=True,
+                ),
+                actions=[
+                    ft.TextButton("閉じる", on_click=lambda e: e.page.pop_dialog())
                 ],
-                tight=True,
-            ),
-            actions=[ft.TextButton("閉じる", on_click=lambda e: e.page.pop_dialog())]
-        ))
+            )
+        )
 
-    open_colab_sentence = LinkedSentence([
-        ("GPUを有効", open_how_to_enable_gpu_dialog),
-        "にしてから",
-        ("すべてのセルを実行", open_how_to_run_cells_dialog),
-        "してください．"
-    ])
-    open_colab_sentence.scroll=ft.ScrollMode.ADAPTIVE
-    open_colab_sentence.expand=True
+    open_colab_sentence = ft.Row(
+        [
+            ft.TextButton(
+                "GPUを有効",
+                on_click=open_how_to_enable_gpu_dialog,
+                style=ft.ButtonStyle(padding=0),
+            ),
+            ft.Text("にしてから"),
+            ft.TextButton(
+                "すべてのセルを実行",
+                on_click=open_how_to_run_cells_dialog,
+                style=ft.ButtonStyle(padding=0),
+            ),
+            ft.Text("してください．"),
+        ],
+        wrap=True,
+        spacing=0,
+    )
     create_url_button = ft.Button("urlを作成", on_click=create_colab_url)
 
     return ft.Column(
-        controls=[# gistは1ユーザーごとに一個持つようにする？そうすると消さなくてよい
-            ft.Text("url作成ボタンを押すと出てくるurlにアクセスしてください（urlは一時間で削除されます）．"),
+        controls=[  # gistは1ユーザーごとに一個持つようにする？そうすると消さなくてよい
+            ft.Text(
+                "url作成ボタンを押すと出てくるurlにアクセスしてください（urlは一時間で削除されます）．"
+            ),
             open_colab_sentence,
             ft.Text("10分ほど経ったら一番下のところに結果が出ます．"),
             create_url_button,
             url_status_control,
-            ft.TextButton(colab_url, url=colab_url, visible=(colab_url != ""))
+            ft.TextButton(colab_url, url=colab_url, visible=(colab_url != "")),
         ]
     )
